7a65d07934c53d51a5fcfae0b2e5c07772277b27
Allen Lee <allenlee@mit.edu>
2019-09-19 15:24:55 -0400

    Fixed spacing for landing

--- src/Landing.js
+++ src/Landing.js
@@ -13,36 +13,42 @@ class Landing extends Component {
     return (
       <MuiThemeProvider theme={fontTheme}>
           <div className={classes.root}>
-            <Typography variant="h2">
-              VidRank
-            </Typography>
-            <Typography variant="h5" style={{width: 800}}>
-              Some description of what the goal of this game is. Can be two lines because there is space.
-            </Typography>
-            <div>
+            <div className={classes.titleSection}>
+              <Typography variant="h2" className={classes.title}>
+                VidRank
+              </Typography>
+              <Typography variant="h5" className={classes.subtitle}>
+                Some description of what the goal of this game is. Can be two lines because there is space.
+              </Typography>
+            </div>
+            <div className={classes.descriptionSection}>
               <Typography variant="h5">
                 How To Play:
               </Typography>
-              <div style={{display: 'flex', flexDirection: 'row'}}>
-                <div>
+              <div className={classes.stepSection}>
+                <div className={classes.singleStep}>
                   <img src={tmp} />
-                  <Typography>
-                    Placeholder Text
+                  <Typography className={classes.stepText}>
+                    1. Watch the reference and unknown videos. Press buttons to
+                    play/pause all videos in a section. Scroll to see all videos.
                   </Typography>
                 </div>
-                <div>
+                <div className={classes.singleStep}>
                   <img src={tmp} />
-                  <Typography>
-                    Placeholder Text
+                  <Typography className={classes.stepText}>
+                    Order unknown videos by how similar each is to the main concept
+                    of the reference set using their labels A-E.
                   </Typography>
                 </div>
-                <div>
-                  <img src={tmp} />
-                  <Typography>
-                    Placeholder Text
+                <div className={classes.singleStep}>
+                  <img src={tmp} className={classes.stepImage}/>
+                  <Typography className={classes.stepText}>
+                    Click "NEXT" when you're ready to move to the next level!
                   </Typography>
                 </div>
               </div>
+            </div>
+            <div className={classes.buttonSection}>
               <Link to="/game" style={{ textDecoration: 'none' }}>
                 <Button variant="contained" size="large" className={classes.startButton}>
                   START
--- src/styles/landing.js
+++ src/styles/landing.js
@@ -1,16 +1,52 @@
 export const styles = theme => ({
   root: {
     display: 'flex',
-    justifyContent: 'center',
-    padding: 16,
-    paddingTop: 96,
-    flexDirection: 'column',
+    justifyContent: 'space-around',
     alignItems: 'center',
+    flexDirection: 'column',
+    width: '100%',
+    height: '100vh',
+  },
+  buttonSection: {
+    display: 'flex',
+  },
+  descriptionSection: {
+    display: 'flex',
+    padding: 16,
+    width: '100vw',
+    flexDirection: 'column',
+  },
+  singleStep: {
+    width: '30%',
   },
   startButton: {
     borderRadius: 16,
     fontSize: 36,
+    width: 570,
     paddingLeft: 32,
     paddingRight: 32,
+  },
+  stepSection: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingBottom: 24,
+  },
+  stepText: {
+    fontSize: 24,
+  },
+  subtitle: {
+    width: '70%',
+    fontSize: 36,
+    paddingBottom: 36,
+  },
+  title: {
+    fontSize: 72,
+    paddingBottom: 16,
+  },
+  titleSection: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
 });
