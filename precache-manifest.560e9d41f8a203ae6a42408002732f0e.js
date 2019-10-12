927f3bb501df95a88c135d6d22494f05110b7e42
Allen Lee <allenlee@mit.edu>
2019-10-12 12:48:30 -0500

    Updates

---
+++ precache-manifest.560e9d41f8a203ae6a42408002732f0e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc289cd3cc088b4f4b994431888ba46a",
+    "url": "/vidrank/index.html"
+  },
+  {
+    "revision": "52b10e51757e8ab2393b",
+    "url": "/vidrank/static/css/2.84bcf17e.chunk.css"
+  },
+  {
+    "revision": "ec2bf57af4a1b26d2e24",
+    "url": "/vidrank/static/css/main.7d6c20f8.chunk.css"
+  },
+  {
+    "revision": "52b10e51757e8ab2393b",
+    "url": "/vidrank/static/js/2.a7aa6e08.chunk.js"
+  },
+  {
+    "revision": "ec2bf57af4a1b26d2e24",
+    "url": "/vidrank/static/js/main.5e95b80e.chunk.js"
+  },
+  {
+    "revision": "316182382b8dd7ad4912",
+    "url": "/vidrank/static/js/runtime~main.1a220d1e.js"
+  },
+  {
+    "revision": "1c5e98f34cd33bcc59e8132996e12fec",
+    "url": "/vidrank/static/media/background.1c5e98f3.jpg"
+  },
+  {
+    "revision": "f5acb3a2a090c0155d8be90f6930edaa",
+    "url": "/vidrank/static/media/button.f5acb3a2.png"
+  },
+  {
+    "revision": "78a619bce505fc3af62b3b81b30fff6b",
+    "url": "/vidrank/static/media/example1.78a619bc.png"
+  },
+  {
+    "revision": "ff5cb0337b3eab0509c6fdccfd0976b0",
+    "url": "/vidrank/static/media/ranking.ff5cb033.png"
+  }
+]);
