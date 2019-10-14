eade5e974a020055b8b7591a2b851c77a26f6263
Alex Andonian <alexandonian@gmail.com>
2019-10-14 00:59:58 -0400

    Add missing imports

--- src/Finish.js
+++ src/Finish.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { Typography} from '@material-ui/core';
 
 
 class Finish extends Component {
 
   render() {
-    return(
+    const { classes } = this.props;
+    return (
       <div className={classes.root}>
         <div className={classes.titleSection}>
           <Typography variant="h2" className={classes.title}>
             You are done! Thank you for playing VidRank!
+        </Typography>
         </div>
       </div>
     );
