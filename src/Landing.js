5c913ad475b334928999d95ec22ec2d78faf4a79
Allen Lee <allenlee@mit.edu>
2019-11-05 11:59:50 -0500

    testing

--- src/Landing.js
+++ src/Landing.js
@@ -69,6 +69,9 @@ class Landing extends Component {
           <Typography variant="h4" className={classes.descriptionTitle}>
               Demo:
           </Typography>
+          <Typography>
+            test
+          </Typography>
           <video className={classes.centerVideo} src={videoDemo} controls>
           </video>
         </div>
