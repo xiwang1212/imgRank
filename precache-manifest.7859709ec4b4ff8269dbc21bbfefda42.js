69ff6de00817728b109e981a8e521e394dbe14c1
camilo <camilofosco@gmail.com>
2019-11-11 01:58:54 -0500

    Updates

---
+++ precache-manifest.7859709ec4b4ff8269dbc21bbfefda42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57395461dbfc7c7c905100ccf9fc59c",
+    "url": "/vidrank/index.html"
+  },
+  {
+    "revision": "aea151b04c05cec08f19",
+    "url": "/vidrank/static/css/2.533d620f.chunk.css"
+  },
+  {
+    "revision": "511fca92fa3f20e67f15",
+    "url": "/vidrank/static/css/main.4bc23541.chunk.css"
+  },
+  {
+    "revision": "aea151b04c05cec08f19",
+    "url": "/vidrank/static/js/2.f07984cd.chunk.js"
+  },
+  {
+    "revision": "511fca92fa3f20e67f15",
+    "url": "/vidrank/static/js/main.6dea0fa1.chunk.js"
+  },
+  {
+    "revision": "316182382b8dd7ad4912",
+    "url": "/vidrank/static/js/runtime~main.1a220d1e.js"
+  }
+]);
