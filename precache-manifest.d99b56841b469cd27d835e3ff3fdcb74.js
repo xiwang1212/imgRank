6c3a37c3819f99a0fb248a81b6a4175671245afd
Allen Lee <allenlee@mit.edu>
2020-02-10 15:57:07 -0500

    Updates

---
+++ precache-manifest.d99b56841b469cd27d835e3ff3fdcb74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298645f0fdc12760abddc98214ca1ce3",
+    "url": "/vidrank/index.html"
+  },
+  {
+    "revision": "98aa4b9eb8b40d0f76a2",
+    "url": "/vidrank/static/css/2.533d620f.chunk.css"
+  },
+  {
+    "revision": "70be933d591c09351912",
+    "url": "/vidrank/static/css/main.4bc23541.chunk.css"
+  },
+  {
+    "revision": "98aa4b9eb8b40d0f76a2",
+    "url": "/vidrank/static/js/2.ec9f2ef8.chunk.js"
+  },
+  {
+    "revision": "70be933d591c09351912",
+    "url": "/vidrank/static/js/main.1076404f.chunk.js"
+  },
+  {
+    "revision": "316182382b8dd7ad4912",
+    "url": "/vidrank/static/js/runtime~main.1a220d1e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vidrank/precache-manifest.1a302079ee661b233a99626c5723993a.js"
+  "/vidrank/precache-manifest.d99b56841b469cd27d835e3ff3fdcb74.js"
 );
 
 self.addEventListener('message', (event) => {
